ec73318abd1a3cd2aa19f458b786e9dc1832abb9
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-02-16 14:15:49 -0500

    Updated from Brython Server: 2/16/2017 2:15:48 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -53,13 +53,13 @@ b=1
 fb=1
 thing=list(range(1,numbers+1))
 while (fizz*f)<= numbers:
-    thing[fizz*f-1]="Fizz"
+    thing[fizz*f-1]="Fizz "
     f=f+1
 while (buzz*b)<= numbers:
-    thing[buzz*b-1]="Buzz"
+    thing[buzz*b-1]="Buzz "
     b=b+1
 while (fizz*buzz*fb)<= numbers:
-    thing[fizz*buzz*fb-1]="FizzBuzz"
+    thing[fizz*buzz*fb-1]="FizzBuzz "
     fb=fb+1
 for x in thing:
     print(x)
